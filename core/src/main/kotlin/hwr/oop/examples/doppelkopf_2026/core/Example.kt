b442a3420f50a519b5f822823e0248668dd51c1b
Stefan Reschke <stefan.reschke@outlook.de>
2026-04-28 10:47:32 +0200

    chore: remove example, prepare empty doppelkopf test

--- core/src/main/kotlin/hwr/oop/examples/doppelkopf_2026/core/Example.kt
+++
@@ -1,5 +0,0 @@
-package hwr.oop.examples.doppelkopf_2026.core
-
-class Example {
-	fun sayHelloTo(string: String) = "Hello $string!"
-}
---
+++ core/src/test/kotlin/hwr/oop/examples/doppelkopf_2026/core/DoppelkopfTest.kt
@@ -0,0 +1,14 @@
+package hwr.oop.examples.doppelkopf_2026.core
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DoppelkopfTest {
+	
+	@Test
+	fun `first test`() {
+		// given
+		// when
+		// then
+	}
+}
